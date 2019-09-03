a3433318fb6decb9e6b1fb6629e909595cd95c43
Juan Riaño <jfrianop@gmail.com>
2019-09-03 14:39:48 -0500

    Add solutions for Sept 3

--- solutions/anagram.test.js
+++ solutions/anagram.test.js
@@ -9,7 +9,11 @@
 //     return false
 
 // write your solution here
-
+function anagram(str1, str2) {
+  str1=str1.split("").sort().join("");
+  str2=str2.split("").sort().join("");
+  return str1===str2;
+}
 
 // tests
 test('valid anagrams', function() {
--- solutions/longestWordLength.test.js
+++ solutions/longestWordLength.test.js
@@ -1,8 +1,17 @@
-// write your pseudocode
-//
+// Pseudocode
+// Input: one string with a sentence
+// Output: length of the longest word in the sentence
+// Instructions:
+//   split string into array of words
+//   sort array using the criteria of the longest word from greatest to smallest
+//   return the lenght of the first element of the array
 
 // write your solution here
-
+function findLongestWordLength(str) {
+  str=str.split(" ");
+  str.sort((a, b) => b.length - a.length);
+  return str[0].length;
+}
 
 // tests
 test('returns longest word length', function() {
