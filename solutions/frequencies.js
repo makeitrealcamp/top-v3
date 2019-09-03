f362e60e0c57213e4b9f40ceda4e75faaf0a45a6
esilvac <esilva1995@gmail.com>
2019-09-02 19:43:39 -0500

    Add pseudocode to excercies

--- solutions/frequencies.js
+++ solutions/frequencies.js
@@ -1,3 +1,20 @@
+/*  Pseudocodigo
+ *
+ *  Entrada: un arreglo de string
+ *  Salida: Retornar un objeto con cada caracter del string y la cantidad de veces que se repite
+ *
+ *  Instrucciones:
+ *    - Eliminar los caracteres vacios o espacios del string de entrada
+ *    - Definir una variable "result" que sea un objeto
+ *    - mientras que i sea menor que la longitud del string entonces
+ *        * definir una variable "char" que sera igual al caracter del string en la posicion "i"
+ *        * buscar la cantidad de veces que se repite el caracter almacenado en char en el string
+ *        * almacenar en la variable result un nuevo objeto que tenga como llave la letra actual, y
+ *        como valo la cantidad de veces que se repite
+ *    - Retornar el resultado
+ */
+
+
 var frequencies = function(string) {
   str = string.replace( /\s/g, '')
   var result = {}
--- solutions/hello.js
+++ solutions/hello.js
@@ -1,3 +1,13 @@
+/*    Pseudocodigo
+ *
+ *    - Entrada un string con un nombre
+ *    - Salida: Devolver la palabra "Hello" seguida del nombre que le dimos por entrada
+ *    - Instrucciones:
+ *        * Devolver la concatenacion de la palabra "Hello" y el string de entrada separados por un
+ *        espacio
+ *
+ * */
+
 function hello_world(name) {
   return "Hello " + name
 }
