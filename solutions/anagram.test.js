040150f34de2a5fceaa69a1c156515c05a815d82
Andrés <ga.alaix564@gmail.com>
2019-09-03 16:47:03 -0500

    Update to Sept-3 Exercises

--- solutions/anagram.test.js
+++ solutions/anagram.test.js
@@ -12,6 +12,12 @@
 
 
 // tests
+function anagram(str1, str2) {
+  str1 = str1.split("").sort().join("");
+  str2 = str2.split("").sort().join("");
+  return str1 === str2;
+};
+
 test('valid anagrams', function() {
   expect(anagram("elvis", "lives")).toBe(true);
   expect(anagram("anagram", "nagaram")).toBe(true);
--- solutions/longestWordLength.test.js
+++ solutions/longestWordLength.test.js
@@ -1,3 +1,12 @@
+/* Pseudocode:
+1. Create a function called findLongestWordLength to contain a string
+2. Convert the str in an array in order to review the length for each str value.
+3. Create a variable wich value will be 0 at the begining, and it will to increase
+in relation with the length of the str.
+4. Create a looo for run the length of the stair and determine which word is the
+largest one.
+5. Compare the lenght values of each word after the iteraction.
+6. Return the largest lenght that represents the largest word. */
 function findLongestWordLength (str){
   str = str.split(" ");
   let longestWord = 0;
